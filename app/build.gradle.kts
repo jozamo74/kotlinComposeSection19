e3b25a117606c38fe319f0c6de0b576bcf022ccb
José Zambrano <jzambra1@tragsa.es>
2024-03-13 12:32:56 +0100

    fix bug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,10 +4,7 @@ plugins {
 
     alias(libs.plugins.ksp)
     alias(libs.plugins.hilt)
-<<<<<<< HEAD
 
-=======
->>>>>>> a4bb5e4469add95a2aa371fe9627d91130333827
 }
 
 
@@ -74,11 +71,8 @@ dependencies {
     implementation(libs.retrofit)
     implementation(libs.convert.gson)
 
-<<<<<<< HEAD
-=======
-//  Dager-Hilt
->>>>>>> a4bb5e4469add95a2aa371fe9627d91130333827
     implementation(libs.hilt.android)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
     ksp(libs.hilt.compiler)
 
     testImplementation(libs.junit)
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,6 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
 
-<<<<<<< HEAD
-=======
-//    Dagger-hilt
->>>>>>> a4bb5e4469add95a2aa371fe9627d91130333827
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.hilt) apply false
 }
